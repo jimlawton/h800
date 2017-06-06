2c8d29793049cc82c47bd66519ebc4166f1a92c9
Jim Lawton <jim.lawton@gmail.com>
2017-06-06 10:18:10 +0100

    Added BS, DS, and WD tests.

--- h800/instructions_test.py
+++ h800/instructions_test.py
@@ -34,6 +34,8 @@ def run_exception_tests(message, testdata):
         assert gotexc == True, "Invalid value should have thrown an exception!"
 
 
+# ######## BA ########
+
 def test_BA_masked_simple():
     testdata = [[BinaryAddMasked, 0, 0, 0o51]]
     run_tests("TEST: BA (masked): simple", testdata)
@@ -91,6 +93,8 @@ def test_BA_unmasked_args_range():
     ]
     run_tests("TEST: BA (unmasked): args range", testdata)
 
+
+# ######## DA ########
 
 def test_DA_masked_simple():
     testdata = [[DecimalAddMasked, 0, 0, 0o41]]
@@ -150,6 +154,8 @@ def test_DA_unmasked_args_range():
     run_tests("TEST: DA (unmasked): args range", testdata)
 
 
+# ######## WA ########
+
 def test_WA_masked_simple():
     testdata = [[WordAddMasked, 0, 0, 0o55]]
     run_tests("TEST: WA (masked): simple", testdata)
@@ -204,5 +210,185 @@ def test_WA_unmasked_args_range():
         [WordAddUnmasked, 1, 1, 0, 1, 0o6515],
         [WordAddUnmasked, 1, 1, 1, 0, 0o6615],
         [WordAddUnmasked, 1, 1, 1, 1, 0o6715]
+    ]
+    run_tests("TEST: WA (unmasked): args range", testdata)
+
+
+# ######## BS ########
+
+def test_BS_masked_simple():
+    testdata = [[BinarySubtractMasked, 0, 0, 0o71]]
+    run_tests("TEST: BA (masked): simple", testdata)
+
+
+def test_BS_masked_invalid_args():
+    testdata = [
+        [BinarySubtractMasked, 0, 32, 0],
+        [BinarySubtractMasked, 2, 0, 0]
+    ]
+    run_exception_tests("TEST: BA (masked): invalid args", testdata)
+
+
+def test_BS_masked_args_range():
+    testdata = [
+        [BinarySubtractMasked, 1, 0, 0o4071],
+        [BinarySubtractMasked, 0, 31, 0o3771],
+        [BinarySubtractMasked, 1, 31, 0o7771]
+    ]
+    run_tests("TEST: BA (masked): args range", testdata)
+
+
+def test_BS_unmasked_simple():
+    testdata = [[BinarySubtractUnmasked, 0, 0, 0, 0, 0o2031]]
+    run_tests("TEST: BA (unmasked): simple", testdata)
+
+
+def test_BS_unmasked_invalid_args():
+    testdata = [
+        [BinarySubtractUnmasked, 2, 0, 0, 0, 0],
+        [BinarySubtractUnmasked, 0, 2, 0, 0, 0],
+        [BinarySubtractUnmasked, 0, 1, 2, 3, 0]
+    ]
+    run_exception_tests("TEST: BA (unmasked): invalid args", testdata)
+
+
+def test_BS_unmasked_args_range():
+    testdata = [
+        [BinarySubtractUnmasked, 0, 0, 0, 0, 0o2031],
+        [BinarySubtractUnmasked, 0, 0, 0, 1, 0o2131],
+        [BinarySubtractUnmasked, 0, 0, 1, 0, 0o2231],
+        [BinarySubtractUnmasked, 0, 0, 1, 1, 0o2331],
+        [BinarySubtractUnmasked, 0, 1, 0, 0, 0o2431],
+        [BinarySubtractUnmasked, 0, 1, 0, 1, 0o2531],
+        [BinarySubtractUnmasked, 0, 1, 1, 0, 0o2631],
+        [BinarySubtractUnmasked, 0, 1, 1, 1, 0o2731],
+        [BinarySubtractUnmasked, 1, 0, 0, 0, 0o6031],
+        [BinarySubtractUnmasked, 1, 0, 0, 1, 0o6131],
+        [BinarySubtractUnmasked, 1, 0, 1, 0, 0o6231],
+        [BinarySubtractUnmasked, 1, 0, 1, 1, 0o6331],
+        [BinarySubtractUnmasked, 1, 1, 0, 0, 0o6431],
+        [BinarySubtractUnmasked, 1, 1, 0, 1, 0o6531],
+        [BinarySubtractUnmasked, 1, 1, 1, 0, 0o6631],
+        [BinarySubtractUnmasked, 1, 1, 1, 1, 0o6731]
+    ]
+    run_tests("TEST: BA (unmasked): args range", testdata)
+
+
+# ######## DS ########
+
+def test_DS_masked_simple():
+    testdata = [[DecimalSubtractMasked, 0, 0, 0o61]]
+    run_tests("TEST: DA (masked): simple", testdata)
+
+
+def test_DS_masked_invalid_args():
+    testdata = [
+        [DecimalSubtractMasked, 0, 32, 0],
+        [DecimalSubtractMasked, 2, 0, 0]
+    ]
+    run_exception_tests("TEST: DA (masked): invalid args", testdata)
+
+
+def test_DS_masked_args_range():
+    testdata = [
+        [DecimalSubtractMasked, 1, 0, 0o4061],
+        [DecimalSubtractMasked, 0, 31, 0o3761],
+        [DecimalSubtractMasked, 1, 31, 0o7761]
+    ]
+    run_tests("TEST: DA (masked): args range", testdata)
+
+
+def test_DS_unmasked_simple():
+    testdata = [[DecimalSubtractUnmasked, 0, 0, 0, 0, 0o2021]]
+    run_tests("TEST: DA (unmasked): simple", testdata)
+
+
+def test_DS_unmasked_invalid_args():
+    testdata = [
+        [DecimalSubtractUnmasked, 2, 0, 0, 0, 0],
+        [DecimalSubtractUnmasked, 0, 2, 0, 0, 0],
+        [DecimalSubtractUnmasked, 0, 1, 2, 3, 0]
+    ]
+    run_exception_tests("TEST: DA (unmasked): invalid args", testdata)
+
+
+def test_DS_unmasked_args_range():
+    testdata = [
+        [DecimalSubtractUnmasked, 0, 0, 0, 0, 0o2021],
+        [DecimalSubtractUnmasked, 0, 0, 0, 1, 0o2121],
+        [DecimalSubtractUnmasked, 0, 0, 1, 0, 0o2221],
+        [DecimalSubtractUnmasked, 0, 0, 1, 1, 0o2321],
+        [DecimalSubtractUnmasked, 0, 1, 0, 0, 0o2421],
+        [DecimalSubtractUnmasked, 0, 1, 0, 1, 0o2521],
+        [DecimalSubtractUnmasked, 0, 1, 1, 0, 0o2621],
+        [DecimalSubtractUnmasked, 0, 1, 1, 1, 0o2721],
+        [DecimalSubtractUnmasked, 1, 0, 0, 0, 0o6021],
+        [DecimalSubtractUnmasked, 1, 0, 0, 1, 0o6121],
+        [DecimalSubtractUnmasked, 1, 0, 1, 0, 0o6221],
+        [DecimalSubtractUnmasked, 1, 0, 1, 1, 0o6321],
+        [DecimalSubtractUnmasked, 1, 1, 0, 0, 0o6421],
+        [DecimalSubtractUnmasked, 1, 1, 0, 1, 0o6521],
+        [DecimalSubtractUnmasked, 1, 1, 1, 0, 0o6621],
+        [DecimalSubtractUnmasked, 1, 1, 1, 1, 0o6721]
+    ]
+    run_tests("TEST: DA (unmasked): args range", testdata)
+
+
+# ######## WD ########
+
+def test_WD_masked_simple():
+    testdata = [[WordDifferenceMasked, 0, 0, 0o75]]
+    run_tests("TEST: WA (masked): simple", testdata)
+
+
+def test_WD_masked_invalid_args():
+    testdata = [
+        [WordDifferenceMasked, 0, 32, 0],
+        [WordDifferenceMasked, 2, 0, 0]
+    ]
+    run_exception_tests("TEST: WA (masked): invalid args", testdata)
+
+
+def test_WD_masked_args_range():
+    testdata = [
+        [WordDifferenceMasked, 1, 0, 0o4075],
+        [WordDifferenceMasked, 0, 31, 0o3775],
+        [WordDifferenceMasked, 1, 31, 0o7775]
+    ]
+    run_tests("TEST: WA (masked): args range", testdata)
+
+
+def test_WD_unmasked_simple():
+    testdata = [[WordDifferenceUnmasked, 0, 0, 0, 0, 0o2035]]
+    run_tests("TEST: WA (unmasked): simple", testdata)
+
+
+def test_WD_unmasked_invalid_args():
+    testdata = [
+        [WordDifferenceUnmasked, 2, 0, 0, 0, 0],
+        [WordDifferenceUnmasked, 0, 2, 0, 0, 0],
+        [WordDifferenceUnmasked, 0, 1, 2, 3, 0]
+    ]
+    run_exception_tests("TEST: WA (unmasked): invalid args", testdata)
+
+
+def test_WD_unmasked_args_range():
+    testdata = [
+        [WordDifferenceUnmasked, 0, 0, 0, 0, 0o2035],
+        [WordDifferenceUnmasked, 0, 0, 0, 1, 0o2135],
+        [WordDifferenceUnmasked, 0, 0, 1, 0, 0o2235],
+        [WordDifferenceUnmasked, 0, 0, 1, 1, 0o2335],
+        [WordDifferenceUnmasked, 0, 1, 0, 0, 0o2435],
+        [WordDifferenceUnmasked, 0, 1, 0, 1, 0o2535],
+        [WordDifferenceUnmasked, 0, 1, 1, 0, 0o2635],
+        [WordDifferenceUnmasked, 0, 1, 1, 1, 0o2735],
+        [WordDifferenceUnmasked, 1, 0, 0, 0, 0o6035],
+        [WordDifferenceUnmasked, 1, 0, 0, 1, 0o6135],
+        [WordDifferenceUnmasked, 1, 0, 1, 0, 0o6235],
+        [WordDifferenceUnmasked, 1, 0, 1, 1, 0o6335],
+        [WordDifferenceUnmasked, 1, 1, 0, 0, 0o6435],
+        [WordDifferenceUnmasked, 1, 1, 0, 1, 0o6535],
+        [WordDifferenceUnmasked, 1, 1, 1, 0, 0o6635],
+        [WordDifferenceUnmasked, 1, 1, 1, 1, 0o6735]
     ]
     run_tests("TEST: WA (unmasked): args range", testdata)
