fb50213979cfb633a54aa9400af8d5f898f596fa
Jim Lawton <jim.lawton@gmail.com>
2017-06-12 17:31:26 +0100

    Change to use instruction field in Card class.

--- tools/parse.py
+++ tools/parse.py
@@ -37,9 +37,10 @@ def main():
     errcount = 0
     linecount = 0
     instrcount = 0
-    instrtab = {}
-    for instruction in h800.instructions.INSTRUCTIONS:
-        instrtab[instruction] = 0
+
+    symtab = {}
+    loc = 0
+    coloc = 0
 
     for filename in args:
         d = h800.arguscard.Deck(file=filename, verbose=opts.verbose)
@@ -54,32 +55,17 @@ def main():
                         print("Skipping card with * in column 8:")
                         print(card.filename, card.linenum, card.line.replace('\n', ''))
                     continue
-                instruction = card.operation.strip().replace(' ', '')
-                if ',' in instruction:
-                    instruction = instruction.split(',')[0]
-                if instruction not in instrtab.keys():
-                    print("*** ERROR: Invalid instruction \"%s\":" % instruction)
+                if card.instruction not in h800.instructions.INSTRUCTIONS:
+                    print("*** ERROR: Invalid instruction \"%s\":" % card.instruction)
                     print("File %s, line %d: %s" % (card.filename, card.linenum, card.line))
                     errcount += 1
                     continue
-                instrtab[instruction] += 1
-                if instruction not in h800.instructions.INSTRUCTIONS:
-                    print("*** ERROR: Invalid instruction \"%s\":" % instruction)
+                if card.instruction not in h800.instructions.INSTRUCTIONS:
+                    print("*** ERROR: Invalid instruction \"%s\":" % card.instruction)
                     print("File %s, line %d: %s" % (card.filename, card.linenum, card.line))
                     errcount += 1
 
     print("\nSource lines: %d" % linecount)
-    print("\nInstruction Frequencies:")
-    print("%-8s %6d  %6.2f%%" % ("Total", instrcount, 100.0))
-    freqtab = OrderedDict(sorted(instrtab.items(), key=lambda t: t[1], reverse=True))
-    for inst in freqtab.keys():
-        if freqtab[inst] != 0:
-            print("%-8s %6d  %6.2f%%" % (inst, freqtab[inst], 100 * float(freqtab[inst])/float(instrcount)))
-
-    print("\nUnused Instructions:")
-    for inst in sorted(instrtab.keys()):
-        if instrtab[inst] == 0:
-            print(inst)
 
     if errcount == 1:
         print("1 error encountered.")
