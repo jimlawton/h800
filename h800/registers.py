facae5e249d7576469d138eac595401a32a24bca
Jim Lawton <jim.lawton@gmail.com>
2017-06-13 15:57:51 +0100

    Store register width and name (if supplied).

--- h800/registers.py
+++ h800/registers.py
@@ -63,11 +63,14 @@ def registerNumber(name):
 
 class Register(object):
     """H-x800 register class."""
-    def __init__(self, data=0):
-        self._data = BitField(0, width=16,
+
+    def __init__(self, data=0, name=None, width=16):
+        self._data = BitField(0, width=width,
                              numbering=BitField.BIT_SCHEME_MSB_1,
                              order=BitField.BIT_ORDER_MSB_LEFT)
         self._data[1:16] = data
+        self._name = name
+        self._width = width
 
     @property
     def value(self):
