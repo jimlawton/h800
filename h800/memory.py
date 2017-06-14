82240313d9f38df170e6db648001bd1efeebc6be
Jim Lawton <jim.lawton@gmail.com>
2017-06-14 15:29:07 +0100

    Add MemoryBank class.

---
+++ h800/memory.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+# H-800/H-1800 Memory
+# ===================
+
+from bitfield import BitField
+from word import word
+
+
+class MemoryBank(object):
+    """H-x800 memory bank class."""
+
+    MEMORY_WIDTH = 56       # 48 data, 6 parity.
+    NUM_BANKS = 4
+    BANK_SIZE = 2048
+
+    def __init__(self, banknum=0):
+        if banknum < 0 or banknum > self.NUM_BANKS:
+            raise ValueError("Invalid value (%d) for memory bank number!" % banknum)
+        self._banknum = banknum
+        self._data = {}
+        for addr in range(BANK_SIZE):
+            self._data[addr] = Word(0, width=self.MEMORY_WIDTH,
+                                    numbering=BitField.BIT_SCHEME_MSB_1,
+                                    order=BitField.BIT_ORDER_MSB_LEFT)
+        self._width = self.MEMORY_WIDTH
+
+    def _checkValue(self, value):
+        if value < 0 or value > (2 ** self._width - 1):
+            raise ValueError("Invalid value for %d-bit word!" % self._width)
+
+    def _checkAddress(self, address):
+        if address < 0 or address > 2047:
+            raise ValueError("Invalid value for address!")
+
+    @property
+    def value(self, address, parity=False):
+        # TODO: support parity.
+        self._checkAddress(address)
+        return int(self._data[address])
+
+    @value.setter
+    def value(self, address, value, parity=False):
+        # TODO: support parity.
+        self._checkAddress(address)
+        self._checkValue(value)
+        if value < 0 or value > (2 ** self._width - 1):
+            raise ValueError("Invalid value for %d-bit word!" % self._width)
+        self._data[address][1:self._width] = value
+
+    @property
+    def width(self):
+        return int(self._width)
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            values = []
+            for i in range(key.start, key.stop):
+                values.append(self._data[i].value)
+        else:
+            return self._data[key].value
+
+    def __setitem__(self, key, value):
+        if isinstance(key, slice):
+            for i in range(key.start, key.stop):
+                self._data[i].value = value
+        else:
+            self._data[key].value = value
+
+    def __len__(self):
+        return len(self._data)
+
+    def __repr__(self):
+        text = ""
+        for addr in self._data:
+            text += "0o%08o" % self._data[addr].value
+        return text
+
+    def clear(self):
+        for addr in self._data:
+            self._data[addr].clear()
+
