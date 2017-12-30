39441ae16830eeec933f8b9b7d1797edd883e7db
Jim Lawton <jim.lawton@gmail.com>
2017-12-30 13:18:29 +0000

    Bump lib version, moving to Python 3.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name="h800",
-    version="0.7",
+    version="0.8",
     description="H-800 Tools",
     url="https://github.com/jimlawton/h800",
     author="Jim Lawton",
