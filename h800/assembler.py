322ace1b6f897eab93c548ecd94a4c674b6cf0a9
Jim Lawton <jim.lawton@gmail.com>
2018-01-27 20:36:59 +0000

    WIP: rewriting.

--- h800/assembler.py
+++ h800/assembler.py
@@ -3,22 +3,25 @@ import time
 from pylog import info, debug, critical
 from parser_record import ParserRecord
 
+from h800.symbol_table import buildSymbolTable
+
 
 class Assembler:
     """H-x800 Assembler class."""
 
-    def __init__(self, context):
-        self.context = context
+    def __init__(self, verbose=False):
+        self._verbose = verbose
+        self._symtab = {}
 
-    def _makeNewRecord(self, srcfile, linenum, line, rectype, label, opcode,
-                       operands, comment):
-        return ParserRecord(self.context, srcfile, linenum, line, label,
-                            opcode, operands, comment)
+    def createSymbolTable(self, srcfile):
+        info("Processing %s" % srcfile)
+        self._symtab, errcount = buildSymbolTable(srcfile, self._symtab,
+                                                  verbose=self._verbose)
 
     def assemble(self, srcfile):
         info("Assembling %s" % srcfile)
-        self.context.srcfile = srcfile
-        self.context.linenum = 0
+        self._srcfile = srcfile
+        self._linenum = 0
         with open(srcfile, 'r') as f:
             lines = f.readlines()
         debug("assemble: file %s, lines %d" % (srcfile, len(lines)))
@@ -29,8 +32,8 @@ class Assembler:
 
     def parse(self, label, opcode, operands):
         try:
-            self.context.log(7, "parse: label='%s' opcode='%s' operands=%s" %
-                             (label, opcode, operands))
+            info(7, "parse: label='%s' opcode='%s' operands=%s" %
+                 (label, opcode, operands))
             # TODO: parse operation here.
         except Exception:
             critical("Exception processing line:")
@@ -46,7 +49,7 @@ class Assembler:
     def resolve(self, maxPasses=10):
         "Resolve a symbol."
         startTime = time.time()
-        self.context.symtab.resolve(maxPasses)
+        # self.context.symtab.resolve(maxPasses)
         endTime = time.time()
         delta = endTime - startTime
         debug("Symbol resolution: %3.2f seconds" % delta)
@@ -56,36 +59,36 @@ class Assembler:
         debug("Updating %d parser records..." % (numRecords))
         nUndefs = nPrevUndefs = 0
         for i in range(maxPasses):
-            self.context.passnum = i + 1
-            self.context.reset()
+            self._passnum = i + 1
+            # self.reset()
             nPrevUndefs = nUndefs
             nUndefs = 0
             undefRecords = []
             for j in range(numRecords):
-                record = self.context.records[j]
+                record = self._records[j]
                 if record.isParseable():
-                    self.context.currentRecord = record
-                    self.context.previousRecord = self.context.records[j-1]
-                    self.context.load(record)
-                    self.context.log(8, "resolve: %s" % (record.srcline))
-                    self.parse(record.label, record.opcode, record.operands)
-                    self.context.records[j] = self.context.currentRecord
+                    self._currentRecord = record
+                    self._previousRecord = self.context.records[j-1]
+                    # self.load(record)
+                    debug("resolve: %s" % (record.srcline))
+                    # self.parse(record.label, record.opcode, record.operands)
+                    self._records[j] = self.context.currentRecord
                     if not record.isComplete():
                         nUndefs += 1
                         undefRecords.append(record)
-            self.context.log(3, "%d incomplete parser records" % (nUndefs))
+            info("%d incomplete parser records" % (nUndefs))
             if nUndefs == 0:
-                self.context.log(3, "all parser records complete")
+                info("All parser records complete")
                 break
             if nUndefs == nPrevUndefs:
                 for urec in undefRecords:
                     urec.error("undefined symbol")
-                    self.context.errors += 1
+                    self._errors += 1
                 self.context.error("no progress resolving parser records, "
                                    "%d undefined records" % nUndefs,
                                    source=False, count=False)
                 break
-        if self.context.debug:
+        if self._verbose:
             endTime = time.time()
             delta = endTime - startTime
             print("Pass 2: %3.2f seconds" % delta)
